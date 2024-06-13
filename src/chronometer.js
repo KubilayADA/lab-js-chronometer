9e5217f17b07a4fc5851fb10e0bb139bd80b156e
Kubilay ADA <kubilayada.26@gmail.com>
2024-06-14 01:45:42 +0200

    opa

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,50 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+  this.currentTime = 0;
+  this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
-  }
+    this.intervalId = setInterval(() => {
+this.currentTime++;
+if (printTimeCallback){
+  printTimeCallback(this.currentTime);
+}
+}, 1000);
+}
 
   getMinutes() {
-    // ... your code goes here
-  }
+let numberOfMins = this.currentTime / 60;
+let roundedMins = Math.floor(numberOfMins);
+    return(roundedMins);
+}
 
   getSeconds() {
-    // ... your code goes here
+let numberOfSecs = this.currentTime % 60;
+return numberOfSecs;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+  let stringValue = String(value);
+  if ( stringValue.length === 1) {
+    return '0' + stringValue;
+  } else {
+    return stringValue;
+  }
   }
 
   stop() {
-    // ... your code goes here
+clearInterval(this.intervalId);
+
   }
 
   reset() {
-    // ... your code goes here
+this.currentTime = 0; 
   }
 
   split() {
-    // ... your code goes here
+let minutes = this.getMinutes();
+let seconds = this.getSeconds();
+return(`${this.computeTwoDigitNumber(minutes)}:${this.computeTwoDigitNumber(seconds)}`);
   }
 }
